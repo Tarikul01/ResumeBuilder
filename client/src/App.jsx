fdd91b33dedf3940e1d327696a560b90d7ff2e20
Himu Ahmed <64636115+himuahmed343@users.noreply.github.com>
2023-04-29 15:29:26 +0600

    template page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ResumePage from "./pages/ResumePage";
 import SignUpPage from "./pages/SignUpPage";
+import Template0 from "./templates/Template0";
 const App = () => {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/resume-templates" element={<ResumePage />} />
 
         <Route path="/registration" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/Template0" element={<Template0 />} />
 
         {/* UnknownRoutes */}
         <Route path="*" element={<h1>Not Found</h1>} />
       </Routes>
-      <Footer />
+      {/* <Footer /> */}
     </Router>
   );
 };
